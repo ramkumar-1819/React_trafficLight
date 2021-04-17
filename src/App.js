a7d8f7d60e17888ae38f2d291e68898eb6661a28
ramkumar-1819 <rohitram1819@gmail.com>
2021-04-18 00:22:59 +0530

    FinalCode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component,useEffect,useState } from 'react';
 
 function App() {
+  const [state,setState]=useState([3000,1000,2000]) //this state actually holds the array of timer of each signal color
+  const[currentLight,setCurrentLight]=useState(0)   //hold which signal we are in [0-red,1-yellow,2-green]
+  
+  //In useeffect we inially set the opactity of selected signal to 1
+  //Then we have a timer initialzed using settimeout which changes the signal based on the signal time we declared
+  
+  useEffect(()=>{
+    document.getElementsByClassName("trafficColor")[0].children[currentLight].style.opacity=1             
+    const timer=setTimeout(()=>{
+                              setCurrentLight((currentLight+1)%3)
+                              document.getElementsByClassName("trafficColor")[0].children[currentLight].style.opacity=0.3
+                              }
+                          ,state[currentLight])
+    return ()=>clearInterval(timer)
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="trafficColor">
+        <div></div>
+        <div></div>
+        <div></div>
     </div>
   );
 }
